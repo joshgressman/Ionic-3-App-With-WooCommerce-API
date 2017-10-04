7ba2156d19c091dbdb50714d2fd6346661c18dbb
joshgressman <joshgressman@gmail.com>
2017-10-04 15:09:49 -0500

    added woocommerce api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//woocommcer api imported via npm install woocommerce-api --save
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
