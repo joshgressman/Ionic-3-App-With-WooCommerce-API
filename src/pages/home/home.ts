8a6a12bfa398aef6387117d910acac2edbecf315
joshgressman <joshgressman@gmail.com>
2017-10-08 09:31:36 -0500

    added product list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,15 +13,21 @@ import * as WC from 'woocommerce-api';
 export class HomePage {
   WooCommerce: any;
   products: any[];
+  page: number
+  moreProducts: any[];
 
   @ViewChild('productSlides') productSlides: Slides;
 
   constructor(public navCtrl: NavController) {
+   this.page = 2;
    this.WooCommerce = WC({
      url: "http://localhost:8888/woocommerce",
      consumerKey: "ck_68671b908096258d3744b8ff2511be0816df7c98",
      consumerSecret: "cs_f394c2fa6678df9f1a3a3feebae63e24ef06662a"
    });
+
+    this.loadMoreProducts()
+
    this.WooCommerce.getAsync("products").then( (data)=> {
      this.products = JSON.parse(data.body).products;
      console.log(this.products);
@@ -38,6 +44,15 @@ export class HomePage {
       }
       this.productSlides.slideNext()
     }, 3000)
+  }
+
+  loadMoreProducts() {
+    this.WooCommerce.getAsync("products?page=" + this.page).then( (data)=> {
+      this.moreProducts = JSON.parse(data.body).products;
+      console.log(this.products);
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
