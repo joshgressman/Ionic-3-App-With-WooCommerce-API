57029ec44e4c247230bc953cf83a1c6644158016
joshgressman <joshgressman@gmail.com>
2017-10-11 16:55:32 -0500

    added api data to category page

--- src/pages/products-by-category/products-by-category.ts
+++ src/pages/products-by-category/products-by-category.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-
-
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-products-by-category',
   templateUrl: 'products-by-category.html',
 })
 export class ProductsByCategoryPage {
+  WooCommerce: any;
+  products: any[];
+  page: number;
+  category: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   //Displays products from the first page
+   this.page = 1;
+   //receives catagory from nav params sent from the MenuPage
+   this.category = this.navParams.get('category');
+    this.WooCommerce = WC({
+      url: "http://localhost:8888/woocommerce",
+      consumerKey: "ck_68671b908096258d3744b8ff2511be0816df7c98",
+      consumerSecret: "cs_f394c2fa6678df9f1a3a3feebae63e24ef06662a"
+    });
+
+    //Get products by category from api
+    this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug).then( (data)=> {
+      this.products = JSON.parse(data.body).products;
+      console.log(this.products);
+    }, (err) => {
+      console.log(err);
+    });
   }
 
   ionViewDidLoad() {
