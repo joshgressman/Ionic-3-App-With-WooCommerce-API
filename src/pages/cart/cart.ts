55a6188cc20b3e6c8920b887d7d6e8746d682039
joshgressman <joshgressman@gmail.com>
2017-10-18 15:13:10 -0500

    added storage to cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-cart',
   templateUrl: 'cart.html',
 })
 export class CartPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cartItems: any[] = [];
+  total: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+    //access storage when storage is ready to be accessed
+    this.storage.ready().then(() => {
+      //access cart data from storage
+      this.storage.get("cart").then((data) => {
+        this.cartItems = data;
+        console.log("items in cart", data);
+        
+      })
+    });
   }
 
 
