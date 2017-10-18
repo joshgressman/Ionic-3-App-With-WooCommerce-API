f958aac8941e645e027700b6e14dd7d33c85b237
joshgressman <joshgressman@gmail.com>
2017-10-18 15:58:47 -0500

    added cart method and styles

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
 @Component({
@@ -9,17 +9,45 @@ import { Storage } from '@ionic/storage';
 export class CartPage {
   cartItems: any[] = [];
   total: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+  showEmptyCartMessage: boolean = false;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public viewCtrl: ViewController) {
+     this.total = 0.0;
     //access storage when storage is ready to be accessed
     this.storage.ready().then(() => {
       //access cart data from storage
       this.storage.get("cart").then((data) => {
         this.cartItems = data;
         console.log("items in cart", data);
-        
+
+      // Add total cost within the cart
+      if(this.cartItems.length > 0) {
+
+        this.cartItems.forEach((item, index) => {
+          this.total = this.total + (item.product.price * item.qty);
+        })
+      } else {
+        this.showEmptyCartMessage = true
+      }
+
       })
     });
   }
 
+   removeFromCart(item, i){
+     let price = item.product.price;
+     let qty = item.qty;
+     this.cartItems.splice(i, 1);
+     this.storage.set("cart", this.cartItems).then(() => {
+      this.total = this.total - (price * qty);
+    });
+     if(this.cartItems.length == 0){
+       this.showEmptyCartMessage = true;
+     }
+   }
+
+   closeModal(){
+     this.viewCtrl.dismiss();
+   }
 
 }
