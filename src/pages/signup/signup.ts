a1aad78611b0bccae26c45d137f35da93ab01864
joshgressman <joshgressman@gmail.com>
2017-10-20 06:53:05 -0500

    added email validation

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-signup',
@@ -11,15 +12,38 @@ export class SignupPage {
   // New user object to be used for [(ngModel)] in form
   newUser: any = {};
   billing_shipping_same: boolean;
+  WooCommerce: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     //Initilaizes the billing address object within newUser object
     this.newUser.billing_address = {};
     this.newUser.shipping_address = {};
     this.billing_shipping_same = false;
+
+    this.WooCommerce = WC({
+      url: "http://localhost:8888/woocommerce",
+      consumerKey: "ck_68671b908096258d3744b8ff2511be0816df7c98",
+      consumerSecret: "cs_f394c2fa6678df9f1a3a3feebae63e24ef06662a"
+    });
+
   }
 
 setBillingToShipping(){
   this.billing_shipping_same = !this.billing_shipping_same;
+}
+
+
+checkEmail(){
+ let validEmail = false;
+ let reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+ //Validates email with regular expression
+ if(reg.test(this.newUser.email)){
+   this.WooCommerce.getAsync('customers/email/' + this.newUser.email).then((data) => {
+     console.log("email data", data);
+   })
+ }
+
 }
 
 }
