574f711321b49ab73cab85060de8809cb5126279
joshgressman <joshgressman@gmail.com>
2017-10-20 06:08:59 -0500

    added user sign up page and form

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -8,11 +8,18 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
+  // New user object to be used for [(ngModel)] in form
+  newUser: any = {};
+  billing_shipping_same: boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //Initilaizes the billing address object within newUser object
+    this.newUser.billing_address = {};
+    this.newUser.shipping_address = {};
+    this.billing_shipping_same = false;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
+setBillingToShipping(){
+  this.billing_shipping_same = !this.billing_shipping_same;
+}
 
 }
