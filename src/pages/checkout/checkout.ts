5fa9809e93e5a11792ad4a28c15ead519d20119e
joshgressman <joshgressman@gmail.com>
2017-10-26 15:13:49 -0500

    added form data in checkout form

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -6,12 +6,31 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'checkout.html',
 })
 export class CheckoutPage {
+  newOrder: any;
+  paymentMethods: any[];
+  paymontMethod: any;
+  billing_shipping_same: boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   this.newOrder = {};
+   this.newOrder.billing_address = {};
+   this.newOrder.shipping_address = {};
+   this.billing_shipping_same = false;
+
+   this.paymentMethods = [
+     {method_id: "bacs", method_title: "Direct Bank Transfer"},
+     {method_id: "cheque", method_title: "Cheque Payment"},
+     {method_id: "cod", method_title: "Cash on Delivery"},
+     {method_id: "paypal", method_title: "PayPal"}
+   ];
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CheckoutPage');
+  setBillingToShipping(){
+    this.billing_shipping_same = !this.billing_shipping_same;
+
+    if(this.billing_shipping_same){
+      this.newOrder.shipping_address = this.newOrder.billing_address;
+    }
   }
 
 }
