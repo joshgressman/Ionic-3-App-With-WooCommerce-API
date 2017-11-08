a9b7d0c5196568b665f88755173be8b9a4bcd5a7
joshgressman <joshgressman@gmail.com>
2017-11-07 18:32:03 -0600

    added completion note

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SignupPage } from '../pages/signup/signup';
 import { MenuPage } from '../pages/menu/menu';
-
+import { OneSignal } from '@ionic-native/onesignal';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +16,7 @@ export class MyApp {
 
 
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public oneSignal: OneSignal) {
     this.initializeApp();
 
   }
@@ -30,5 +30,23 @@ export class MyApp {
     });
   }
 
+  useOneSignal(){
+    //OneSignal Push Notifications
+    this.oneSignal.startInit('b8c3d416-9a36-4c30-81c1-072cb0a41ad6', '73895551261');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe(() => {
+     // do something when notification is received
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe(() => {
+      // do something when a notification is opened
+    });
+
+    this.oneSignal.endInit();
+  }
+
+  //app completion, do not have a live WP URL
 
 }
